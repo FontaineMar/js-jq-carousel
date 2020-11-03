b075132554c1b4a8330b45a3629353ae878c7844
marco <odri.fo@gmail.com>
2020-11-03 17:21:06 +0100

    quasi finito

---
+++ main.js
@@ -0,0 +1,20 @@
+  $('.prev, .next').click(function(){
+
+      var immaginiEl = $('img.active')
+      immaginiEl.removeClass('active')
+      if(immaginiEl.hasClass('last')){
+        $('.first').addClass('active')
+      } else {immaginiEl.next().addClass('active')
+      }
+  })
+
+
+  $('.prev, .next').click(function(){
+      var first = $('first > i')
+      var circleEl = $('i.active')
+      circleEl.removeClass('active')
+      if(circleEl.hasClass('last')){
+        first.addClass('active')
+      } else {circleEl.next().addClass('active')
+      }
+  })
