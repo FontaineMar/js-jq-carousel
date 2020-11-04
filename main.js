5e9ed41fc19493487b68b56cfdd7ef6a4e151d18
marco <odri.fo@gmail.com>
2020-11-04 21:32:14 +0100

    finito

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-  $('.prev, .next').click(function(){
+$(document).ready(function(){
+
+  $('.next').click(function(){
       var circleEl = $('i.active')
       var immaginiEl = $('img.active')
       circleEl.removeClass('active')
@@ -9,3 +11,16 @@
         circleEl.next().addClass('active')
       }
   })
+
+  $('.prev').click(function(){
+      var circleEl = $('i.active')
+      var immaginiEl = $('img.active')
+      circleEl.removeClass('active')
+      immaginiEl.removeClass('active')
+      if(immaginiEl.hasClass('first')){
+        $('img.last , i.last').addClass('active')
+      } else {immaginiEl.prev().addClass('active')
+        circleEl.prev().addClass('active')
+      }
+  })
+})
